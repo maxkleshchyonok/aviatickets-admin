87c594ae359c9f6b4a48cfa35d7f3ba47e6ac7af
ilik-sh <i.shyshparonak@gmail.com>
2024-01-18 18:16:31 +0300

    feat: create passenger table for dropdown

---
+++ src/app/bookings/components/PassengerTable/passenger-table.comp.tsx
@@ -0,0 +1,35 @@
+import { Typography, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
+import React from 'react'
+import { PassengerDto } from 'src/aviatickets-submodule/libs/types/passenger.dto'
+
+type Props = {
+    passengers: PassengerDto[]
+}
+
+export default function PassengerTable({passengers}: Props) {
+  return (
+    <>
+    <Typography variant="h6" gutterBottom component="div">
+        Passengers
+    </Typography>
+    <Table size="small" aria-label="flights" sx={{marginBottom: 2}}>
+        <TableHead>
+        <TableRow>
+            <TableCell>First Name</TableCell>
+            <TableCell>Last Name</TableCell>
+        </TableRow>
+        </TableHead>
+        <TableBody>
+        {passengers.map((passenger) => (
+            <TableRow key={passenger.id}>
+                <TableCell component="th" scope="row">
+                    {passenger.firstName}
+                </TableCell>
+                <TableCell>{passenger.lastName}</TableCell>
+            </TableRow>
+        ))}
+        </TableBody>
+    </Table>
+</>
+  )
+}
